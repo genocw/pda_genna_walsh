4be215ad4ac14e722a19cfd026815006ff4ec3b8
Genna Walsh <gennawalsh@gmail.com>
2018-11-23 18:22:38 +0000

    UI integration tests

--- Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,70 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  // UI integration test 1
+  it('should update display of running total when number clicked', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12');
+  })
+
+  // UI integration test 2
+  it('should update display when operation clicked with result of operation', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3');
+  })
+
+  // UI integration test 3
+  it('should chain multiple operations together', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#operator_add')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
+  // UI integration test 4: positive numbers
+  it('should give expected output when handling positive numbers', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2');
+  })
+  // UI integration test 4: negative numbers
+  it('should give expected output when handling negative numbers', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-3');
+  })
+  // UI integration test 4: decimals
+  it('should give expected output when handling decimals', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1.5');
+  })
+
+  // UI integration test 5
+  it('should give expected output when number is divided by zero', function () {
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number6')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('Not a number');
+  })
+
 });
